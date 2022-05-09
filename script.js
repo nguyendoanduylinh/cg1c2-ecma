ba043fd703793d54976d1498b949bb4e32c4d48c
nguyendoanduylinh <nguyen.doan.duy.linh@gmail.com>
2022-05-09 10:16:06 +0700

    done

---
+++ script.js
@@ -0,0 +1,6 @@
+let officialName = prompt("Tên chính thức của Javascript là gì?")
+if (officialName == "ECMAScript") {
+    alert("Chính xác!");
+} else {
+    alert("Sai rồi! Câu trả lời là ECMAScript!");
+}
